b320bb493f1faba1bc6e836cf8b0e8fbd209e775
jay4497 <jay4497@126.com>
2024-07-31 17:04:22 +0800

    dev: watermark

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,9 @@
 const canvas = document.getElementById('work-canvas')
 const context = canvas.getContext('2d')
 let sourceImage = null
+let watermarkSource = null
+let watermarkSourceWidth;
+let watermarkSourceHeight;
 
 const elFinalImage = document.getElementById('work-image')
 const initArea = document.getElementById('init-area')
@@ -17,6 +20,10 @@ const elWatermarkImgBtn = document.getElementById('watermark-img-btn')
 const elWatermarkText = document.getElementById('watermark-text')
 const elWatermarkPosition = document.getElementById('watermark-position')
 const elWatermarkFontSize = document.getElementById('watermark-font-size')
+const elWatermarkImgUploadBtn = document.getElementById('watermark-img-upload')
+const elWatermarkImgInput = document.getElementById('watermark-img')
+const elWatermarkImgPosition = document.getElementById('watermark-img-position')
+const elWatermarkImgSize = document.getElementById('watermark-img-size')
 
 elFileInput.addEventListener('change', upload)
 elUploadBtn.addEventListener('click', () => {
@@ -45,6 +52,16 @@ elWatermarkBtn.addEventListener('click', () => {
 elWatermarkText.addEventListener('change', watermarkUpdate)
 elWatermarkPosition.addEventListener('change', watermarkUpdate)
 elWatermarkFontSize.addEventListener('change', watermarkUpdate)
+
+elWatermarkImgBtn.addEventListener('click', () => {
+    toggleMenu('.watermark-img-menu')
+})
+elWatermarkImgInput.addEventListener('change', watermarkImgUpload)
+elWatermarkImgUploadBtn.addEventListener('click', () => {
+    elWatermarkImgInput.click()
+})
+elWatermarkImgPosition.addEventListener('change', watermarkImgUpdate)
+elWatermarkImgSize.addEventListener('change', watermarkImgUpdate)
 
 function upload(e) {
     const files = e.target.files
@@ -79,8 +96,8 @@ function watermarkUpdate() {
     drawSourceImage()
     context.font = fontSize + 'px "Microsoft YaHei",sans-serif'
     const textWidth = context.measureText(text).width
-    console.log(textWidth, fontSize)
     let _x = 10, _y = parseInt(fontSize) + 10
+    let points = []
     if (position == 1) {
         _x = canvas.width - 10 - textWidth
     }
@@ -95,11 +112,105 @@ function watermarkUpdate() {
         _x = canvas.width / 2 - (textWidth / 2)
         _y = canvas.height / 2 - (fontSize / 2)
     }
+    if (position == 5) {
+        let _flag = true
+        _x = -canvas.width / 2
+        _y = -canvas.height / 2
+        while (_y < canvas.height + (canvas.height / 2)) {
+            points.push({
+                _x,
+                _y
+            })
+            _x += 50 + parseInt(textWidth)
+            if (_x >= canvas.width + (canvas.width / 2)) {
+                _y += 100 + parseInt(fontSize)
+                if (_flag) {
+                    _x = -canvas.width / 2 - textWidth / 2
+                } else {
+                    _x = -canvas.width / 2
+                }
+                _flag = !_flag
+            }
+        }
+        context.translate(canvas.width / 2, canvas.height / 2)
+        context.rotate(30 * Math.PI / 180)
+        context.translate(-canvas.width / 2, -canvas.height / 2)
+    }
 
-    context.strokeStyle = '#fff'
-    context.strokeText(text, _x, _y, textWidth)
-    context.fillStyle = '#333333cc'
-    context.fillText(text, _x, _y, textWidth)
+    if (points.length <= 0) {
+        points = [{
+            _x, _y
+        }]
+    }
+    context.globalAlpha = 0.3
+    for (let i = 0; i < points.length; i++) {
+        let _point = points[i]
+        context.strokeStyle = '#fff'
+        context.strokeText(text, _point._x, _point._y, textWidth)
+        context.fillStyle = '#333'
+        context.fillText(text, _point._x, _point._y, textWidth)
+    }
+    context.setTransform(1, 0, 0, 1, 0, 0)
+    context.globalAlpha = 1
+    elFinalImage.setAttribute('src', getDataURL())
+}
+
+function watermarkImgUpload(e) {
+    const files = e.target.files
+    if (files.length === 0) {
+        return
+    }
+    const file = files[0]
+    const imageUrl = URL.createObjectURL(file)
+    const image = new Image()
+    image.src = imageUrl
+    image.onload = () => {
+        URL.revokeObjectURL(imageUrl)
+
+        const _canvas = document.createElement('canvas')
+        _canvas.width = image.width
+        _canvas.height = image.height
+        const _ctx = _canvas.getContext('2d')
+        _ctx.globalAlpha = 0.3
+        _ctx.drawImage(image, 0, 0, image.width, image.height)
+        const _watermark = _canvas.toDataURL()
+        const _image = new Image()
+        _image.src = _watermark
+        _image.onload = () => {
+            watermarkSource = _image
+            watermarkSourceWidth = _image.width
+            watermarkSourceHeight = _image.height
+            watermarkImgUpdate()
+        }
+    }
+}
+
+function watermarkImgUpdate() {
+    if (!watermarkSource) {
+        return
+    }
+    const position = elWatermarkImgPosition.value
+    const size = elWatermarkImgSize.value
+    const scale = size / 100
+    watermarkSource.width = watermarkSourceWidth * scale
+    watermarkSource.height = watermarkSourceHeight * scale
+    let _x = 10, _y = 10
+    if (position == 1) {
+        _x = canvas.width - 10 - watermarkSource.width
+    }
+    if (position == 2) {
+        _y = canvas.height - 10 - watermarkSource.height
+    }
+    if (position == 3) {
+        _x = canvas.width - 10 - watermarkSource.width
+        _y = canvas.height - 10 - watermarkSource.height
+    }
+    if (position == 4) {
+        _x = canvas.width / 2 - watermarkSource.width / 2
+        _y = canvas.height / 2 - watermarkSource.height / 2
+    }
+    drawSourceImage()
+    context.drawImage(watermarkSource, _x, _y, watermarkSource.width, watermarkSource.height)
     elFinalImage.setAttribute('src', getDataURL())
 }
 
